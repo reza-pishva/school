5301c38e31a2ea6bd6cb6412fbd2cbf3464d2aaa
Reza Pishva <rpishva@gmail.com>
2023-04-12 19:50:09 +0330

    created exam types route

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,9 @@ Route::get('/school/grade/{id}',[GradeController::class,'grade']);
 Route::get('/school/grade/remove/{id}',[GradeController::class,'remove']);
 Route::put('/school/grade/update/{id}',[GradeController::class,'update']);
 Route::post('/school/grade/store',[GradeController::class,'store']);
+
+Route::get('/school/exam/exam-types',[ExamTypeController::class,'examTypes']);
+Route::get('/school/exam/exam-type/{id}',[ExamTypeController::class,'examType']);
+Route::get('/school/exam/exam-type/remove/{id}',[ExamTypeController::class,'remove']);
+Route::put('/school/exam/exam-type/update/{id}',[ExamTypeController::class,'update']);
+Route::post('/school/exam/exam-type/store',[ExamTypeController::class,'store']);
