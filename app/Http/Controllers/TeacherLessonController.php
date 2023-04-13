ee48a021d7b07d75d5ce44bc354a37d88dc4b6f2
Reza Pishva <rpishva@gmail.com>
2023-04-13 21:52:28 +0330

    added controller

--- app/Http/Controllers/TeacherLessonController.php
+++ app/Http/Controllers/TeacherLessonController.php
@@ -3,8 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\TeacherLesson;
 
 class TeacherLessonController extends Controller
 {
-    //
+    public function lessons(){
+        $lessons = TeacherLesson::all();
+        return TeacherLesson::all();
+    }
+    public function lesson($id){
+        $lessons = TeacherLesson::find($id);
+        return TeacherLesson::find($id);
+    }
+    public function remove($id){
+        $result = TeacherLesson::find($id)->delete();     
+    }
+    public function update(Request $request , $id){
+        $lessons = TeacherLesson::find($id);
+        $lessons->update(['year'=>$request->year,
+                          'lesson_id'=>$request->lesson_id]);
+        return $lessons;  
+    }
+    public function store(Request $request){
+        TeacherLesson::create(['year'=>$request->year,
+                               'lesson_id'=>$request->lesson_id]); 
+        return $request->all();
+    }
 }
--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -11,7 +11,6 @@ use App\Grade;
 class Exam extends Model
 {
     use HasFactory;
-    use SoftDeletes;
 
     protected $table='exams';
     protected $guarded=[];
