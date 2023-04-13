b4d5322720167dda7493fe7a58b49b87db725a4e
Reza Pishva <rpishva@gmail.com>
2023-04-13 18:49:46 +0330

    added controller for classroom table

---
+++ app/Http/Controllers/ClassRoomController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\ClassRoom;
+
+class ClassRoomController extends Controller
+{
+    public function classrooms(){
+        $classroom = ClassRoom::all();
+        return $classroom;
+    }
+    public function student($id){
+        $classroom = ClassRoom::find($id);
+        return $classroom;
+    }
+    public function remove($id){
+        $result = ClassRoom::find($id)->delete();     
+    }
+    public function update(Request $request , $id){
+        $classroom = ClassRoom::find($id);
+        $classroom->update(['year' => $request->year,
+                          'name' => $request->name, 
+                          'grade_id' => $request->grade_id]);
+        return $classroom;  
+    }
+    public function store(Request $request){
+        ClassRoom::create(['year' => $request->year,
+                         'name' => $request->name, 
+                         'grade_id' => $request->grade_id]); 
+        return $request->all();
+    }
+}
