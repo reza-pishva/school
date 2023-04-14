921ace9bda75b8cf5abc4fd44c35a6dc0510805d
Reza Pishva <rpishva@gmail.com>
2023-04-14 18:41:18 +0330

    added migration for exam and students

---
+++ database/migrations/2023_04_14_150545_create_exam_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exam_students', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('year');
+            $table->bigInteger('date_shamsi');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('exam_id');
+            $table->foreign('exam_id')->references('id')->on('exams')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exam_students');
+    }
+}
