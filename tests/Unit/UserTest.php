778e76f2f2cf94340849e7f1f11938d9d6660d09
Reza Pishva <rpishva@gmail.com>
2023-05-08 11:16:19 +0330

    added some tests to user

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,12 +1,7 @@
 <?php
  
  namespace Tests\Feature;
- 
- use Illuminate\Foundation\Testing\RefreshDatabase;
- use Illuminate\Foundation\Testing\WithoutMiddleware;
  use Illuminate\Foundation\Testing\DatabaseTransactions;
-//  use Illuminate\Http\UploadedFile;
-//  use Illuminate\Support\Facades\Storage;
  use Tests\TestCase;
  
 class UserTest extends TestCase
@@ -31,5 +26,35 @@ class UserTest extends TestCase
         $response = $this->post('/api/school/user/store', $data);
         $response->assertStatus(200);
         $this->assertDatabaseHas('users', $data);
+    }
+    public function test_update_user()
+    {
+        $data = [
+            'f_name'=>'1',
+            'l_name'=>'2',
+            'father_name'=>'3',
+            'national_code'=>'4',
+            'gender'=>'1',
+            'role'=>'1',
+            'email'=>'rpishva999@gmail.com',
+            'password'=>'1111111',
+        ];
+        $response = $this->put('/api/school/user/update/16', $data);
+        $response->assertStatus(200);
+    }
+    public function test_get_user_by_id()
+    {
+        $response = $this->get('/api/school/user/16');
+        $response->assertStatus(200);
+    }
+    public function test_get_users()
+    {
+        $response = $this->get('/api/school/user/users');
+        $response->assertStatus(200);
+    }
+    public function test_get_user_profile()
+    {
+        $response = $this->get('/api/school/user-profile/16');
+        $response->assertStatus(200);
     }
 }
